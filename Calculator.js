754b9f77fc5f6045e6bee8654e94ab6579d80f7d
Divyanshu <baranwaldiv9210@gmail.com>
2025-03-31 19:35:36 +0530

    Requirement 4 : Support Custom Delimiters following the format //[delimiter]\n[numbers...]

--- Calculator.js
+++ Calculator.js
@@ -1,15 +1,23 @@
-// Requirement 3 : Support New Line as a Delimiter
+// Requirement 4 : Support Custom Delimiters following the format //[delimiter]\n[numbers...]
 
 
 function add(numbers) {
     if (!numbers) return 0;
 
     // Add new line as a delimitter too
-    let newAccepetanceRegex = /,|\n/;
-    let numArray = numbers.split(newAccepetanceRegex).map(Number);
+    let delimiter = /,|\n/;
+    let match = numbers.match(/^\/\/(.+)\n/); // Check if the input string has the extra custom delimiters
+    if (match) {
+        delimiter = new RegExp(match[1]);  // Create a new regex using the custom delimiter
+        numbers = numbers.slice(match[0].length);  // Remove the "//;\n" part from numbers (if it carries that)
+    }
+
+    // The delimiter must have been updated with the new requirement
+    let numArray = numbers.split(delimiter).map((el) => Number(el));
     return numArray.reduce((sum, num) => sum + num, 0);
 }
 
 // Test cases
 console.log(add("1,2,3,4,5")); // 15
 console.log(add("1\n2,3")); // 6
+console.log(add("//;\n1;2"));
