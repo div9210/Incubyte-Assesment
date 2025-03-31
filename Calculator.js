38894dbec3ce5c516674cc4c3265dfa04985f4a7
Divyanshu <baranwaldiv9210@gmail.com>
2025-03-31 19:24:14 +0530

    Requirement 3 : Support New Line as a Delimiter

--- Calculator.js
+++ Calculator.js
@@ -1,13 +1,15 @@
-// Requirement 2 : Handle Unknown Amount of Numbers
+// Requirement 3 : Support New Line as a Delimiter
 
-// Note : The same function is fully capable to handle multiple comma seperated args
 
 function add(numbers) {
     if (!numbers) return 0;
 
-    let numArray = numbers.split(",").map(Number);
+    // Add new line as a delimitter too
+    let newAccepetanceRegex = /,|\n/;
+    let numArray = numbers.split(newAccepetanceRegex).map(Number);
     return numArray.reduce((sum, num) => sum + num, 0);
 }
 
 // Test cases
 console.log(add("1,2,3,4,5")); // 15
+console.log(add("1\n2,3")); // 6
